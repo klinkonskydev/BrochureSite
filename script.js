4f96da05682b80e3d152208798a521f60a5c7c23
Matheus Gabriel Klinkonsky <M01001101K01001011@gmail.com>
2021-07-16 10:06:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-const contact = document.querySelector('#contac')
 const dateTitle = document.querySelector("#date-responsive")
-
 
 function timeZone(time){
     const today = new Date()
     hour = today.getHours()
     minute = today.getMinutes()
     second = today.getSeconds()
-
     
     const timeText = `Time: ${hour}:${minute}:${second}` 
 
     return dateTitle.innerHTML = `${timeText}`;
 }
-
 setInterval(timeZone, 500)
